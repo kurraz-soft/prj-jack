c1ac1d749ec90a1675492743cd5a4002a83b6dbd
kurraz <elfd01@gmail.com>
2017-09-09 19:27:28 +0300

    new random helper funcs

--- modules/game/helpers/RandomHelper.php
+++ modules/game/helpers/RandomHelper.php
@@ -16,4 +16,43 @@ class RandomHelper
         $rand = mt_rand($min * $base, $max * $base);
         return $rand / $base;
     }
+
+    /**
+     * @param $value_weights
+     * @return mixed
+     *
+     * [$value => $weight]
+     */
+    public static function randByWeights($value_weights)
+    {
+        $sum_weights = array_sum($value_weights);
+        $value_weights_normalized = [];
+        foreach ($value_weights as $value => $weight)
+        {
+            $value_weights_normalized[$value] = ($weight / (float)$sum_weights) * 100;
+        }
+
+        $rand = mt_rand(0, 100);
+        foreach ($value_weights_normalized as $value => $weight)
+        {
+            if($rand <= $weight)
+            {
+                return $value;
+            }
+        }
+
+        return array_keys($value_weights)[0];
+    }
+
+    /**
+     * Returns $var by $chance or else returns $else
+     * @param $var
+     * @param $else
+     * @param int $chance percent max 100
+     * @return mixed
+     */
+    public static function randChooseVar($var, $else, $chance)
+    {
+        return static::randByWeights([$var => $chance, $else => 100 - $chance]);
+    }
 }
