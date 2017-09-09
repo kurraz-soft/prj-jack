2118bddfe6980f70b1181e4a83855df18fb2588b
kurraz <elfd01@gmail.com>
2017-09-09 19:42:29 +0300

    add obedience difficulty property

--- modules/game/models/game_data/aura/Obedience.php
+++ modules/game/models/game_data/aura/Obedience.php
@@ -8,8 +8,23 @@ namespace app\modules\game\models\game_data\aura;
 
 
 use app\modules\game\models\game_data\base\BaseAuraAttribute;
+use app\modules\game\models\game_data\base\IAutoSerializable;
+use app\modules\game\models\game_data\serializators\AutoSerializator;
 
-class Obedience extends BaseAuraAttribute
+class Obedience extends BaseAuraAttribute implements IAutoSerializable
 {
+    public $difficulty = 14;
 
+    public function serializableParams()
+    {
+        return [
+            'value' => '',
+            'difficulty' => '',
+        ];
+    }
+
+    protected function getSerializator()
+    {
+        return new AutoSerializator($this);
+    }
 }
