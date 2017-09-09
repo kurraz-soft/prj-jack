ddcf3a152d0bdfc68f14afe5ad4450efc316d30c
kurraz <elfd01@gmail.com>
2017-09-09 18:08:29 +0300

    comments, some explanations

--- modules/game/models/game_data/attributes/FertilityFemale.php
+++ modules/game/models/game_data/attributes/FertilityFemale.php
@@ -9,10 +9,18 @@ namespace app\modules\game\models\game_data\attributes;
 
 use app\modules\game\models\game_data\base\BaseGameDataList;
 
+/**
+ * Class FertilityFemale
+ * @package app\modules\game\models\game_data\attributes
+ *
+ * Плодовитость
+ * Определяет возможность и вероятность деторождения
+ */
 class FertilityFemale extends BaseGameDataList
 {
     /**
-     * @var int
+     * @var float
+     * Множитель, чем больше тем выше шанс забеременеть
      */
     public $value = 1;
     /**
