253ff8dc19f6c2981acd4a17b746e120f6db99d2
kurraz <elfd01@gmail.com>
2017-09-10 20:57:19 +0300

    get set value through get set func in common skills

--- modules/game/models/game_data/base/CommonSkill.php
+++ modules/game/models/game_data/base/CommonSkill.php
@@ -9,27 +9,45 @@ namespace app\modules\game\models\game_data\base;
 
 use app\modules\game\models\game_data\serializators\BasicValueSerializator;
 
+/**
+ * Class CommonSkill
+ * @package app\modules\game\models\game_data\base
+ *
+ * @property $value
+ */
 abstract class CommonSkill extends BaseSkill
 {
-    public $value = 0;
+    use TMagicSetGet;
+
+    public $_value = 0;
     protected $_serializator = null;
 
     protected function getSerializator()
     {
         if($this->_serializator == null)
         {
-            $this->_serializator = new BasicValueSerializator($this);
+            $this->_serializator = new BasicValueSerializator($this, '_value');
         }
         return $this->_serializator;
     }
 
     public function getValue()
     {
-        return $this->value;
+        return $this->_value;
     }
 
     public function setValue($new_value)
     {
-        $this->value = $new_value;
+        $this->_value = clamp($new_value, $this->getMinValue(), $this->getMaxValue());
+    }
+
+    public function getMaxValue()
+    {
+        return max(array_keys($this->valueNames()));
+    }
+
+    public function getMinValue()
+    {
+        return min(array_keys($this->valueNames()));
     }
 }
--- modules/game/models/game_data/serializators/BasicValueSerializator.php
+++ modules/game/models/game_data/serializators/BasicValueSerializator.php
@@ -17,27 +17,29 @@ class BasicValueSerializator implements ISerializator
      * @var IValuable
      */
     public $obj;
+    public $var_name;
 
-    public function __construct($obj)
+    public function __construct($obj, $var_name = 'value')
     {
         if(!($obj instanceof IValuable))
             throw new Exception("Serializator exception");
 
         $this->obj = $obj;
+        $this->var_name = $var_name;
     }
 
     public function serialize()
     {
         return [
-            'value' => $this->obj->getValue(),
+            $this->var_name => $this->obj->getValue(),
         ];
     }
 
     public function unserialize($serialized_data)
     {
-        if(isset($serialized_data['value']))
+        if(isset($serialized_data[$this->var_name]))
         {
-            $this->obj->setValue($serialized_data['value']);
+            $this->obj->setValue($serialized_data[$this->var_name]);
         }
     }
 }
